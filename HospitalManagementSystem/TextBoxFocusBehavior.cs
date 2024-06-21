00a81d9314f2d8b3a36408f54720741655e34426
Noel Stark <noel.k.stark@gmail.com>
2024-06-21 22:53:36 +0200

    Added DependencyProperty for Address Template

---
+++ HospitalManagementSystem/TextBoxFocusBehavior.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace HospitalManagementSystem.TextBoxFocusBehavior
+{
+    public static class TextBoxFocusBehavior
+    {
+        public static readonly DependencyProperty PlaceHolderTextProperty =
+            DependencyProperty.RegisterAttached(
+                "PlaceHolderText",
+                typeof(string),
+                typeof(TextBoxFocusBehavior),
+                new PropertyMetadata(string.Empty, OnPlaceHolderTextChanged));
+
+        public static string GetPlaceholderText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(PlaceHolderTextProperty);
+        }
+        public static void SetPlaceholderText(DependencyObject obj, string value)
+        {
+            obj.SetValue(PlaceHolderTextProperty, value);
+        }
+        private static void OnPlaceHolderTextChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            if(obj is TextBox textbox)
+            {
+                textbox.GotFocus += TextBox_GotFocus;
+                textbox.LostFocus += TextBox_LostFocus;
+            }
+        }
+        
+        private static void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textbox && textbox.Text == GetPlaceholderText(textbox))
+            {
+                textbox.Text = string.Empty;
+            }
+        }
+
+        private static void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(sender is TextBox textbox && textbox.Text == string.Empty)
+            {
+                textbox.Text = GetPlaceholderText(textbox);
+            }
+        }
+        
+    }
+}
--- HospitalManagementSystem/ViewModels/MainViewModel.cs
+++ HospitalManagementSystem/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Input;
 using HospitalManagementSystem.Commands;
 
@@ -10,6 +11,75 @@ namespace HospitalManagementSystem.ViewModels
         public event PropertyChangedEventHandler? PropertyChanged;
 
         private string currentViewKey;
+
+        #region Binding Attributes
+        private string _firstname = "First Name", _lastname = "Last Name", _ssn = "YYYYMMDD-XXXX", _email = "example@gmail.com", _confirmemail = "example@gmail.com", _phone = "Phone Number";
+        private string _country = "Country", _street = "Street", _postalcode = "12345", _city = "City", _state = "State";
+        public string FirstName
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+
+        public string SSN
+        {
+            get { return _ssn; }
+            set { _ssn = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public string ConfirmEmail
+        {
+            get { return _confirmemail; }
+            set { _confirmemail = value; }
+        }
+
+        public string Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+
+        public string Country
+        {
+            get { return _country; }
+            set { _country = value; }
+        }
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+
+        public string Street
+        {
+            get { return _street; }
+            set { _street = value; }
+        }
+        public string PostalCode
+        {
+            get { return _postalcode; }
+            set { _postalcode = value; }
+        }
+        public string State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+
+        #endregion
+
         public string CurrentViewKey
         {
             get => currentViewKey;
@@ -28,6 +98,7 @@ namespace HospitalManagementSystem.ViewModels
             CurrentViewKey = "PersonalInformation";
         }
 
+        
         private void SwitchView(object x)
         {
             CurrentViewKey = CurrentViewKey == "PersonalInformation" ? "Address" : "PersonalInformation";
